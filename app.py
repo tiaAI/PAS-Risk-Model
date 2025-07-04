b23ad4d044133453a74a15a1ce54918d813b1046
tiaAI <107823134+tiaAI@users.noreply.github.com>
2025-07-04 09:38:32 +0800

    app.py

--- app.py
+++ app.py
@@ -25,38 +25,38 @@ feature_names = [
 st.title("PAS Risk Model")
 
 # age: numerical input
-Gestational_age = st.number_input("Gestational Age (year):", min_value=0, max_value=100, value=25)
+Gestational_age = st.number_input("Gestational Age (year):", min_value=0, max_value=100, value=29)
 
 # sex: categorical selection
-Maternal_age = st.number_input("Maternal Age (day):",  min_value=0, max_value=500, value=205)
+Maternal_age = st.number_input("Maternal Age (day):",  min_value=0, max_value=500, value=259)
 
 # cp: categorical selection
 Gravidity = st.number_input("Gravidity:", min_value=0, max_value=50, value=3)
 
 # trestbps: numerical input
-Parturition = st.number_input("Parturition:", min_value=0, max_value=50, value=1)
+Parturition = st.number_input("Parturition:", min_value=0, max_value=50, value=0)
 
 # trestbps: numerical input
 Prior_C_sections_number = st.number_input("Prior C-Sections Number:", min_value=0, max_value=50, value=0)
 
 # chol: numerical input
-Uterine_surgery_number = st.number_input("Uterine Surgery Number:", min_value=0, max_value=50, value=1)
+Uterine_surgery_number = st.number_input("Uterine Surgery Number:", min_value=0, max_value=50, value=2)
 
 # fbs: categorical selection
-Cervical_canal_length = st.number_input("Cervical Canal Length (mm):", min_value=0, max_value=200, value=22)
+Cervical_canal_length = st.number_input("Cervical Canal Length (mm):", min_value=0, max_value=200, value=31)
 
 # restecg: categorical selection
-Uterine_anteroposterior_diameter_ratio = st.number_input("Uterine Anteroposterior Diameter Ratio:", min_value=0.0, max_value=50.0, value=1.5)
+Uterine_anteroposterior_diameter_ratio = st.number_input("Uterine Anteroposterior Diameter Ratio:", min_value=0.0, max_value=50.0, value=1.2)
 
 
 # thalach: numerical input
-Placental_abnormal_vasculature_diameter = st.number_input("Placental Abnormal Vasculature Diameter (mm):", min_value=0., max_value=50.0, value=3.4)
+Placental_abnormal_vasculature_diameter = st.number_input("Placental Abnormal Vasculature Diameter (mm):", min_value=0., max_value=50.0, value=4.1)
 
 # thalach: numerical input
-Placental_abnormal_vasculature_area = st.number_input("Placental Abnormal Vasculature Area (mm2):",  min_value=0.0, max_value=50.0, value=9.6)
+Placental_abnormal_vasculature_area = st.number_input("Placental Abnormal Vasculature Area (mm2):",  min_value=0.0, max_value=50.0, value=4.5)
 
 # exang: categorical selection
-Intraplacental_dark_T2_band_area = st.number_input("Intraplacental Dark T2WI Band Area (mm2):", min_value=0.0, max_value=50.0, value=7.5)
+Intraplacental_dark_T2_band_area = st.number_input("Intraplacental Dark T2WI Band Area (mm2):", min_value=0.0, max_value=50.0, value=5.2)
 
 # Process inputs and make predictions
 feature_values = [Gestational_age,Maternal_age,Gravidity,Parturition,Prior_C_sections_number,Uterine_surgery_number,Cervical_canal_length,Uterine_anteroposterior_diameter_ratio,Placental_abnormal_vasculature_diameter,Placental_abnormal_vasculature_area,Intraplacental_dark_T2_band_area]
